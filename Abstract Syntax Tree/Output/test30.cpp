e9c7f06f38af4a82c8b84d59b21cdec1a8a39005
NinaadRao <ninaadrrao@gmail.com>
2019-04-18 11:54:33 +0530

    ast

---
+++ Abstract Syntax Tree/Output/test30.cpp
@@ -0,0 +1,22 @@
+int main(){ int a; { { int b; } int c; }
+}
+
+Preorder traversal of the abstract syntax tree
+( main  int (    a (    b  c )))
+
+
+
+Tree
+(main)
+ \__(int)
+ \__( )
+     \__(a)
+     \__( )
+         \__(b)
+         \__(c)
+
+
+
+Parsing is successful
+
+
